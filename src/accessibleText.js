4bfeb97771aef6f8168a1113380dec49e305340a
Ben Boyle <benjamins.boyle@gmail.com>
2011-08-28 17:25:56 +1000

    code passes jslint check

--- src/accessibleText.js
+++ src/accessibleText.js
@@ -1,20 +1,30 @@
 /**
  * jQuery plugin that returns the text nodes within the target element, combined/concatenated with any alt text.
  */
-$.fn.accessibleText = function () {
-       result = '';
-       $(this).contents().each(function(){
-               switch (this.nodeType) {
-                       case 3: // text
-                               result += $(this).text();
-                       break;
-                       case 1: // element
-                               if ($(this).prop('nodeName') == 'IMG' && typeof $(this).attr('alt') != 'undefined') {
-                                       result += $(this).attr('alt');
-                               }
-                       break;
-                       default: break; // otherwise ignore
-               }
-       });
-       return result;
-};
+
+if ( jQuery !== "undefined" ) {
+(function( $ ) {
+	"use strict";
+
+	$.fn.accessibleText = function () {
+
+		var result = '';
+
+		$(this).contents().each(function(){
+			switch (this.nodeType) {
+			case 3: // text
+				result += $(this).text();
+			break;
+			case 1: // element
+				if ($(this).prop('nodeName') === 'IMG' && typeof $(this).attr('alt') !== 'undefined') {
+				result += $(this).attr('alt');
+				}
+			break;
+			default: break; // otherwise ignore
+			}
+		});
+		return result;
+	};
+
+}( jQuery ));
+}
