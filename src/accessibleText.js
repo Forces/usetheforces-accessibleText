d4193cec9bad2817daffc86ee40ae1d33c82d329
Ben Boyle <benjamins.boyle@gmail.com>
2011-08-28 17:16:01 +1000

    initial tests pass

--- src/accessibleText.js
+++ src/accessibleText.js
@@ -2,19 +2,19 @@
  * jQuery plugin that returns the text nodes within the target element, combined/concatenated with any alt text.
  */
 $.fn.accessibleText = function () {
-	result = '';
-	$(this).contents().each(function(){
-		switch ($(this).prop('nodeType')) {
-			case 3: // text
-				result += $(this).text();
-			break;
-			case 1: // element
-				if ($(this).prop('nodeName') == 'IMG' && typeof $(this).attr('alt') != 'undefined') {
-					result += $(this).attr('alt');
-				}
-			break;
-			default: break; // otherwise ignore
-		}
-	});
-	return result;
+       result = '';
+       $(this).contents().each(function(){
+               switch (this.nodeType) {
+                       case 3: // text
+                               result += $(this).text();
+                       break;
+                       case 1: // element
+                               if ($(this).prop('nodeName') == 'IMG' && typeof $(this).attr('alt') != 'undefined') {
+                                       result += $(this).attr('alt');
+                               }
+                       break;
+                       default: break; // otherwise ignore
+               }
+       });
+       return result;
 };
