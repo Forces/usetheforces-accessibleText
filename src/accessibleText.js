c33ec9bce72e68b4c131bf19eca0d17d82664618
Andrew Ramsden <andrew@irama.org>
2011-10-24 21:17:24 +1000

    Updated such that the 'value' of inputs is also included (good for buttons). Also now returns the correct value if called on an img or input directly.

--- src/accessibleText.js
+++ src/accessibleText.js
@@ -2,34 +2,29 @@
  * jQuery plugin that returns the text nodes within the target element, combined/concatenated with any alt text.
  */
 
-if ( jQuery !== "undefined" ) {
+if ( jQuery !== 'undefined' ) {
 (function( $ ) {
-	"use strict";
+	'use strict';
 
 	$.fn.accessibleText = function() {
-
-		return $.map( this.contents(), function( domElement ) {
-
-			if ( domElement.nodeType === 3 ) {
-				return domElement.data;
-				
-			} else if ( domElement.nodeType === 1 ) {
-
-				var $element = $( domElement );
-
-				if ( $element.is( "img" ) === true ) {
-					return $element.attr( "alt" );
-
-				} else if ( $element.find( "img[alt]" ).length > 0 ) {
-					return $element.accessibleText();
-
-				} else {
-					return $element.text();
+		if (this.is('img')) {
+			return this.attr( 'alt' );
+		} else if (this.is('input')) {
+			return this.attr( 'value' );
+		} else {
+			return $.map( this.contents(), function( domElement ) {
+				if ( domElement.nodeType === 3 ) {
+					return domElement.data;
+				} else if ( domElement.nodeType === 1 ) {
+					var $element = $( domElement );
+					if ( $element.is( 'img, input' ) || $element.find( 'img[alt], input[value]' ).length > 0 ) {
+						return $element.accessibleText();
+					} else {
+						return $element.text();
+					}
 				}
-			}
-
-		}).join( "" );
-
+			}).join( '' );
+		}
 	};
 
 }( jQuery ));
